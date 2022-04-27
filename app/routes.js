09e4305007773fc338af51ff0ee6e90af52d1b01
Jesse Yuen <contactjesseyuen@gmail.com>
2022-04-27 16:37:46 +0100

    Set up send and close flows, more templating on manage section

--- app/routes.js
+++ app/routes.js
@@ -3,17 +3,44 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-//Frot door
+//Front door
 router.post('/contact-type-answer', function (req, res) {
 
-let contactTypeChoice = req.session.data['contact-type']
+  let contactType = req.session.data.contactType
 
-if (contactTypeChoice === 'complaint') {
-    res.redirect('/complaints')
-} else {
-    //res.redirect('/job_alerts2/create-success-email')
-}
+  if (contactType === 'complaint') {
+      res.redirect('/complaints')
+  } else {
+      //res.redirect('/job_alerts2/create-success-email')
+  }
 
+})
+
+//Complaints
+
+// Send them to manage
+
+router.post('/complaint-type-answer', function (req, res) {
+
+  let complaintType = req.session.data.complaintType
+
+  if (complaintType === 'View an existing complaint') {
+      res.redirect('/manage')
+  } else {
+      //res.redirect('/job_alerts2/create-success-email')
+  }
+
+})
+
+// Catch all route
+
+router.get('*', function(req, res, next){
+  
+  if (req.query){
+    res.locals.query = req.query
+  }
+
+  next()
 })
 
 module.exports = router
